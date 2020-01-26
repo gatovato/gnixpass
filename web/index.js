9bc577c06a8ec048f1747135a048363e81a911f7
Shane <44628141+gatovato@users.noreply.github.com>
2020-01-26 00:33:40 -0700

    passed passes into select form-group

--- web/index.js
+++ web/index.js
@@ -7,13 +7,22 @@ async function getPasses() {
   // Inside a function marked 'async' we can use the 'await' keyword.
 
   let n = await eel.getPasses()(); // Must prefix call with 'await', otherwise it's the same syntax
-  for(i=0;i<n.length;i++){
-    passes = document.getElementById('pass-files');
-    passes.innerHTML += n[i];
-  };
+  if(n.length > 0){
+    passes = document.getElementById("pass-files");
+    var option = document.createElement("option");
+    option.text = "Add a new pass";
+    passes.add(option);
+    for(i=0;i<n.length;i++){
+      var option = document.createElement("option");
+      option.text = n[i];
+      passes.add(option);
+    }
+  }
   if(n.length == 0){
-    passes = document.getElementById('pass-files');
-    passes.innerHTML = 'Add a new pass';
+    passes = document.getElementById("pass-files");
+    var option = document.createElement("option");
+    option.text = "Add a new pass";
+    passes.add(option);
   }
 }
 

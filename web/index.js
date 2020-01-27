e3aeb8c20952a0eafe43a6f9c6e641eaadf6cbff
Shane <44628141+gatovato@users.noreply.github.com>
2020-01-26 17:01:02 -0700

    added to index

--- web/index.js
+++ web/index.js
@@ -26,4 +26,25 @@ async function getPasses() {
   }
 }
 
+function createPass(){
+  var passForm = `
+  <div>
+      <label for="passName">Choose a file or create a new console</label>
+      <input id="passName" class="form-control" type="text" placeholder="Pass file Name">
+      <label for="password">Password</label>
+      <input type="password" class="form-control" id="password" placeholder="Password">
+      <label for="confpassword">Confirm Password</label>
+      <input type="repassword" class="form-control" id="confpassword" placeholder="Confirm Password">
+  </div>
+  `
+  document.getElementById('template').innerHTML = passForm;
+}
+
 getPasses();
+createPass();
+
+dropDown = document.getElementById("pass-files");
+dropDown.addEventListener('change',function(event){
+    var myValue = event.srcElement.value;
+    console.log(myValue);
+});

6041310b15ef46ccabc39ff3a3c6f67fd21d0b6c
Shane <44628141+gatovato@users.noreply.github.com>
2020-01-26 23:34:09 -0700

    switchable UI between create store and authenticate

--- web/index.js
+++ web/index.js
@@ -2,6 +2,32 @@
 //eel.getPasses()(x => myList = x)
 
 
+function createPass(){
+  var passForm = `
+  <div>
+      <label for="passName" class="mt-1">Store name</label>
+      <input id="passName" class="form-control" type="text" placeholder="Store name">
+      <label for="password" class="mt-1">Password</label>
+      <input type="password" class="form-control" id="password" placeholder="Password">
+      <label for="confpassword" class="mt-1">Confirm Password</label>
+      <input type="repassword" class="form-control" id="confpassword" placeholder="Confirm Password">
+      <button type="button" class="btn btn-primary mt-3">Save</button>
+  </div>
+  `
+  document.getElementById('template').innerHTML = passForm;
+}
+
+function authenticate(){
+  var authForm = `
+  <div>
+      <label for="password" class="mt-1">Password</label>
+      <input type="password" class="form-control" id="password" placeholder="Password">
+      <button type="button" class="btn btn-primary mt-3">Open</button>
+  </div>
+  `
+  document.getElementById('template').innerHTML = authForm;
+}
+
 
 async function getPasses() {
   // Inside a function marked 'async' we can use the 'await' keyword.
@@ -9,42 +35,34 @@ async function getPasses() {
   let n = await eel.getPasses()(); // Must prefix call with 'await', otherwise it's the same syntax
   if(n.length > 0){
     passes = document.getElementById("pass-files");
-    var option = document.createElement("option");
-    option.text = "Add a new pass";
-    passes.add(option);
     for(i=0;i<n.length;i++){
       var option = document.createElement("option");
       option.text = n[i];
       passes.add(option);
     }
+    var option = document.createElement("option");
+    option.text = "Add a new pass";
+    passes.add(option);
+    authenticate();
   }
   if(n.length == 0){
     passes = document.getElementById("pass-files");
     var option = document.createElement("option");
     option.text = "Add a new pass";
     passes.add(option);
+    createPass();
   }
 }
 
-function createPass(){
-  var passForm = `
-  <div>
-      <label for="passName">Choose a file or create a new console</label>
-      <input id="passName" class="form-control" type="text" placeholder="Pass file Name">
-      <label for="password">Password</label>
-      <input type="password" class="form-control" id="password" placeholder="Password">
-      <label for="confpassword">Confirm Password</label>
-      <input type="repassword" class="form-control" id="confpassword" placeholder="Confirm Password">
-  </div>
-  `
-  document.getElementById('template').innerHTML = passForm;
-}
 
 getPasses();
-createPass();
 
 dropDown = document.getElementById("pass-files");
 dropDown.addEventListener('change',function(event){
     var myValue = event.srcElement.value;
-    console.log(myValue);
+    if(myValue !="Add a new pass"){
+      authenticate();
+    }else{
+      createPass();
+    }
 });
